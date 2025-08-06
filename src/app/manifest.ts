649fdc99c5158600e2399e029f9936632073188a
 <marieswaran@elatre.com>
2025-08-06 10:41:13 +0530

    feat: Add mainfest file

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Dr.E',
+        short_name: 'Dr.E',
+        description: 'Elevating the future of dental care with Dr. Eduardo',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#f5f5f5',
+        theme_color: '#658192',
+        icons: [
+            {
+                src: '/icon.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/apple-icon.png',
+                sizes: '180x180',
+                type: 'image/png',
+            },
+        ],
+    };
+}
