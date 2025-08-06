45c1a7ceb5c3289ac828fdfc4af3f49e925298c5
 <marieswaran@elatre.com>
2025-08-06 10:50:46 +0530

    feat: Add favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,6 +11,11 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#658192',
         icons: [
             {
+                src: '/favicon.ico',
+                sizes: '32x32',
+                type: 'image/x-icon',
+            },
+            {
                 src: '/icon.png',
                 sizes: '192x192',
                 type: 'image/png',
